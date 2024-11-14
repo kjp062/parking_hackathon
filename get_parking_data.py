d5dd946aa3a0e17f59c6174b140823c1ee3e257d
kjp062 <kjp62@georgetown.edu>
2024-11-14 16:29:11 -0500

    removed unnecessary user agent

--- get_parking_data.py
+++ get_parking_data.py
@@ -6,9 +6,8 @@ import pprint
 pp = pprint.PrettyPrinter(depth=4)
 
 url = "https://api.exactpark.com/api/v2/arlington/status/zones"
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:129.0) Gecko/20100101 Firefox/129.0"}
 
-parkingApiResponse =  re.get(url, headers=headers, timeout=(10, 27))
+parkingApiResponse =  re.get(url, timeout=(10, 27))
 parkingDict = parkingApiResponse.json()
 #print(parkingObject)
 
