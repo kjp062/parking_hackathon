076015e08f4163acbaacb38538642c73e4360108
kjp062 <kjp62@georgetown.edu>
2024-11-14 16:47:03 -0500

    update csv path

--- get_meter_data.py
+++ get_meter_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-meterData = pd.read_csv("C:\\Users\\keper\\Documents\\Coding\\hackathon\\Parking_Meters.csv")
+meterData = pd.read_csv("Parking_Meters.csv")
 
 def get_meter_data():
     return meterData
